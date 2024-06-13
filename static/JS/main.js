668c5dadb5bb998bb8556aa196b137bd1c123494
name pls <e-mail pls>
2024-06-13 23:41:17 +0500

    navbar

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hero = document.querySelector(".hero");
   hero.style.animationDelay = "0.5s";
+
+  const toggle = document.querySelector("#menubar");
+  const navContainer = document.querySelector("#nav-container");
+  const mainBg = document.querySelector(".main-bg");
+
+  toggle.addEventListener("click", function () {
+    if (navContainer.classList.contains("show")) {
+      navContainer.classList.remove("show");
+      mainBg.classList.remove("nav-active-bg");
+      hero.classList.remove("hide");
+    } else {
+      navContainer.classList.add("show");
+      mainBg.classList.add("nav-active-bg");
+      hero.classList.add("hide");
+    }
+  });
 });
